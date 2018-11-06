8b467864cb5cc5de77aab6d3cd3fcd4287264fd8
ljw1412 <506644791@qq.com>
2018-11-06 17:55:20 +0800

    add login block.

---
+++ client/src/modules/user/components/loginBlock.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-block">
+    <div class="login-block__username-warpper">
+      <i-input></i-input>
+    </div>
+    <div class="login-block__password-warpper">
+      <i-input></i-input>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+</style>
