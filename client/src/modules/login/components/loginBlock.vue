4253cc21780b6cc90309cb53c10abaaa50153687
ljw1412 <506644791@qq.com>
2018-11-09 18:35:04 +0800

    登录界面

--- client/src/modules/login/components/loginBlock.vue
+++ client/src/modules/login/components/loginBlock.vue
@@ -1,30 +1,42 @@
 <template>
   <div class="login-block">
-    <div class="login-block__username input-warpper">
-      <i-input v-model="user.name"
-        placeholder="请输入用户名"></i-input>
-    </div>
-    <div class="login-block__password input-warpper">
-      <i-input v-model="user.password"
-        type="password"
-        placeholder="请输入密码"></i-input>
-    </div>
-    <template v-if="isRegister">
-      <div class="login-block__retype-password input-warpper">
-        <i-input v-model="user.retypePassword"
-          type="password"
-          placeholder="请再次输入密码"></i-input>
-      </div>
-      <div class="login-block__email input-warpper">
-        <i-input v-model="user.email"
-          type="email"
-          placeholder="请输入邮箱"></i-input>
-      </div>
-    </template>
-    <div class="login-block__button input-warpper">
-      <span @click="onRegisterClick">{{registerText}}</span>
-      <i-button type="primary">{{buttonText}}</i-button>
-    </div>
+    <i-form class="login-block__warpper"
+      :model="user">
+      <transition name="slide-fade-1">
+        <i-form-item v-if="isLoaded">
+          <i-input v-model="user.name"
+            placeholder="请输入用户名"></i-input>
+        </i-form-item>
+      </transition>
+      <transition name="slide-fade-2">
+        <i-form-item v-if="isLoaded">
+          <i-input v-model="user.password"
+            type="password"
+            placeholder="请输入密码"></i-input>
+        </i-form-item>
+      </transition>
+      <transition name="slide-fade-1">
+        <i-form-item v-if="isRegisterInputShow">
+          <i-input v-model="user.retypePassword"
+            type="password"
+            placeholder="请再次输入密码"></i-input>
+        </i-form-item>
+      </transition>
+      <transition name="slide-fade-2">
+        <i-form-item v-if="isRegisterInputShow">
+          <i-input v-model="user.email"
+            type="email"
+            placeholder="请输入邮箱"></i-input>
+        </i-form-item>
+      </transition>
+      <transition name="slide-fade-3">
+        <div v-if="isLoaded"
+          class="login-block__button">
+          <span @click="onRegisterClick">{{registerText}}</span>
+          <i-button type="primary">{{buttonText}}</i-button>
+        </div>
+      </transition>
+    </i-form>
   </div>
 </template>
 
@@ -40,7 +52,9 @@ export default {
   },
   data() {
     return {
+      isLoaded: false,
       isRegister: false,
+      isRegisterInputShow: false,
       user: {
         name: '',
         password: '',
@@ -51,8 +65,14 @@ export default {
   },
   methods: {
     onRegisterClick() {
+      this.isRegisterInputShow = !this.isRegisterInputShow
       this.isRegister = !this.isRegister
     }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isLoaded = true
+    }, 900)
   }
 }
 </script>
@@ -60,12 +80,13 @@ export default {
 <style lang="scss" scoped>
 .login-block {
   width: 240px;
-  .input-warpper {
-    padding-top: 20px;
+  margin: 0 auto;
+  &__warpper {
+    overflow: hidden;
   }
   &__button {
     display: flex;
-    justify-content: flex-end;
+    justify-content: space-between;
     align-items: center;
     span {
       cursor: pointer;
@@ -75,5 +96,27 @@ export default {
       }
     }
   }
+}
+@mixin slide-fade($in-delay, $out-delay) {
+  &-enter-active {
+    transition: all 0.5s ease $in-delay;
+  }
+  &-leave-active {
+    transition: all 0.5s cubic-bezier(1, 0.5, 0.8, 1) $out-delay;
+  }
+  &-enter,
+  &-leave-to {
+    transform: translateX(240px);
+    opacity: 0;
+  }
+}
+.slide-fade-1 {
+  @include slide-fade(0s, 0.3s);
+}
+.slide-fade-2 {
+  @include slide-fade(0.3s, 0s);
+}
+.slide-fade-3 {
+  @include slide-fade(0.6s, 0s);
 }
 </style>
--- client/src/modules/login/index.vue
+++ client/src/modules/login/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="login">
-    <login-block></login-block>
+    <transition name="expand">
+      <div v-if="isReady"
+        class="login__warpper">
+        <login-block></login-block>
+      </div>
+    </transition>
   </div>
 </template>
 
@@ -9,9 +14,48 @@ import loginBlock from './components/loginBlock'
 export default {
   components: {
     loginBlock
+  },
+  data() {
+    return {
+      isReady: false
+    }
+  },
+  mounted() {
+    setTimeout(() => {
+      this.isReady = true
+    }, 300)
   }
 }
 </script>
 
 <style lang="scss" scoped>
+.login {
+  height: 100%;
+  background-color: #ccc;
+  position: relative;
+  &__warpper {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    width: 300px;
+    text-align: center;
+    background-color: rgba(255, 255, 255, 0.92);
+    padding: 50px 0 20px 0;
+    border-radius: 4px;
+    min-height: 220px;
+  }
+}
+
+.expand-enter {
+  min-height: 0;
+  padding: 0;
+}
+.expand-enter-to {
+  min-height: 220px;
+  padding: 50px 0 20px 0;
+}
+.expand-enter-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.64, 2.14);
+}
 </style>
