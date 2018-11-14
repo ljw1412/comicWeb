be70f31c6db1428f953fcbc0d85ec9c7990eaa20
ljw1412 <506644791@qq.com>
2018-11-14 20:00:15 +0800

    完善日历

--- client/src/components/calendar.vue
+++ client/src/components/calendar.vue
@@ -20,13 +20,36 @@ export default {
     firstDay: { type: String, default: 'SUN' }
   },
   data() {
-    return {}
+    return {
+      currentMonthList: [],
+      monthMap: {},
+      today: moment().format('YYYY-MM-DD'),
+      currentMonth: moment().format('YYYY-MM')
+    }
   },
   computed: {
     weekNameList() {
       return this.firstDay === 'SUN'
         ? ['日', '一', '二', '三', '四', '五', '六']
         : ['一', '二', '三', '四', '五', '六', '日']
+    }
+  },
+  methods: {
+    calculate(month) {
+      if (this.monthMap[month]) {
+        this.currentMonthList = this.monthMap[month]
+        return
+      }
+      const dayCount = dayInMonth(month)
+      const startWeekOfMonth = startWeekOfMonth(month)
+    },
+    dayInMonth(month) {
+      return moment(month, 'YYYY-MM').dayInMonth()
+    },
+    startWeekOfMonth(month) {
+      return moment(month, 'YYYY-MM')
+        .startOf('month')
+        .week()
     }
   }
 }
