397de3b87a3150b3c0a6eaac973363d30589f7f1
ljw1412 <506644791@qq.com>
2018-11-23 16:45:08 +0800

    优化 searchModal

--- client/src/modules/home/pages/searchModal.vue
+++ client/src/modules/home/pages/searchModal.vue
@@ -2,7 +2,14 @@
   <modal-view class="search-modal"
     resizable
     icon="ios-search"
-    :title="title">
+    :title="title"
+    :windowHeight="windowHeight"
+    :minWidth="minWidth"
+    :minHeight="minHeight"
+    :width="width"
+    :height="height"
+    :x="x"
+    :y="y">
 
   </modal-view>
 </template>
@@ -10,9 +17,25 @@
 <script>
 import ModalView from '../../../components/modalView'
 export default {
+  props: {
+    // 浏览器有效的宽高
+    windowWidth: Number,
+    windowHeight: Number,
+    // modal最小的宽高
+    minWidth: { type: Number, default: 300 },
+    minHeight: { type: Number, default: 400 },
+    // modal 初始化时的宽高
+    width: { type: Number, default: 300 },
+    height: { type: Number, default: 400 },
+    // modal 初始化时的位置
+    x: { type: Number, default: 0 },
+    y: { type: Number, default: 0 }
+  },
+
   components: {
     ModalView
   },
+
   data() {
     return {
       title: '搜索'
