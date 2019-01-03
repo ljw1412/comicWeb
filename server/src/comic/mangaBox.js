03ac4f2c25e8771a79951456cc3485b678bb71e4
ljw1412 <506644791@qq.com>
2019-01-03 22:28:02 +0800

    imageList

--- server/src/comic/mangaBox.js
+++ server/src/comic/mangaBox.js
@@ -8,7 +8,8 @@ async function browsePage(url) {
     method: 'GET',
     headers: {
       'User-Agent':
-        'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
+        'Mozilla/5.0 (Linux; U; Android 8.0.0; zh-cn;  MI8 Build/JRO03L) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30 XiaoMi/MiuiBrowser/1.0'
+      // 'Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
     }
   })
   return res.text()
@@ -25,36 +26,53 @@ async function getComicList() {
     .get()
 }
 
-async function getChapterList(url) {
+async function getComicInfo(url) {
   const sourceCode = await browsePage(url)
-  // console.log(sourceCode)
-
   const $ = cheerio.load(sourceCode)
-  // const chapterWrapper = $('.episodes_list .episodes_item a')
-  // const comicTitle = $('.episodes_title').text()
-  // const comicAuthor = $('.episodes_author').text()
-  // const chapterList = chapterWrapper
-  //   .map((index, el) => {
-  //     const chapterUrl = homeUrl + $(el).attr('href')
-  //     const title = $(el)
-  //       .find('.episodes_item_detail .episodes_strong_text')
-  //       .text()
-  //     return { title, url: chapterUrl }
-  //   })
-  //   .get()
-  // return {
-  //   title: comicTitle,
-  //   author: comicAuthor,
-  //   list: chapterList
-  // }
+  let info = $('script:contains(@context)').html()
+  info = JSON.parse(info)
+
+  const comic = {}
+  info['@graph'].forEach(item => {
+    if (item['@type'] === 'Book') {
+      comic.name = item.name
+      comic.author = item.author.map(element => element.name).join()
+      comic.descroption = item.descroption
+      comic.cover = item.image
+      comic.datePublished = item.datePublished
+      comic.url = item.url
+    }
+    if (item['@type'] === 'ItemList') {
+      comic.list = item.itemListElement.map(element =>
+        Object.assign(element.item, { position: element.position })
+      )
+    }
+  })
+  return comic
+}
+
+// 获得具体章节下的图片
+async function getImageList(url) {
+  const sourceCode = await browsePage(url)
+  const $ = cheerio.load(sourceCode)
+  let imageStr = $('script:contains(portrait_img)').html()
+  imageStr = imageStr
+    .replace(/[ \n\r]/g, '')
+    .replace(/,]/g, ']')
+    .match(/\[.*?\]/g)
+  if (imageStr) {
+    return imageStr.map(item => JSON.parse(item))
+  }
+  return []
 }
 
 const startSpider = async website => {
   const comicList = await getComicList()
   if (comicList.length) {
   }
-  const chapterList = await getChapterList(comicList[0])
-  return chapterList
+  const comicInfo = await getComicInfo(comicList[1])
+  const comicImageList = await getImageList(homeUrl + comicInfo.list[0].url)
+  return comicImageList
 }
 
 module.exports = startSpider
